785bdd78e7c794923fb629d751ff68f3ab424cd5
Rifat Alfasya <rifatalfasya21@gmail.com>
2025-07-16 01:26:36 +0800

    Setup databse connection to the server.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const { createServer } = require('http');
 require('dotenv').config();
 
+const pool = require('./models/users.model');
+
 const app = require('./app');
 
 const PORT = process.env.PORT;
 
 const server = createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server is listening at port ${PORT}...`);
-});
+async function connectServer() {
+    await pool.connect();
+    
+    server.listen(PORT, () => {
+        console.log(`Server is listening at port ${PORT}...`);
+    });    
+}
+
+connectServer();
