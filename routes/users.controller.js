026e045d227b61b66f6a7535e335c3c014c61212
Rifat Alfasya <rifatalfasya21@gmail.com>
2025-07-16 01:38:18 +0800

    Adding database feature to the controller and handling missing data.

--- routes/users.controller.js
+++ routes/users.controller.js
@@ -27,8 +27,20 @@ async function getAllUsers(req, res) {
 async function getUserById(req, res) {
     const { id } = req.params;
     try {
+        const result = await pool.query(
+            `SELECT * FROM users WHERE id = $1`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message : 'User not found.',
+            });
+        }
+
         res.status(200).json({
             message : `Retrieving user with id: ${id}`,
+            data : result.rows[0],
         });
     } catch(err) {
         console.error(err);
