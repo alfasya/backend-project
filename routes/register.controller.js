3ac6f8fb75501fd548fc271209620449e71e22cf
Rifat Alfasya <rifatalfasya21@gmail.com>
2025-07-16 13:06:03 +0800

    Create register controller.

---
+++ routes/register.controller.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+
+const pool = require('../models/users.model');
+
+async function registerController(req, res) {
+    try {
+        const { username, password } = req.body;
+
+        const encrypted = await bcrypt.hash(password, 10);
+
+        await pool.query('BEGIN');
+
+        await pool.query(
+            `INSERT INTO accoounts (username) VALUES ($1)`,
+            [username]
+        );
+
+        const account_id = await pool.query(
+            `SELECT id FROM accounts WHERE username = $1`,
+            [username]
+        );
+
+        await pool.query(
+            `INSERT INTO encrypted_password (encrypted_password, account_id) VALUES ($1, $2)`,
+            [encrypted, account_id]
+        );
+
+        await pool.query('COMMIT');
+
+        res.status(201).json({
+            message : 'New account created successfully.',
+        });
+
+    } catch(err) {
+        await pool.query('ROLLBACK');
+        console.error(err);
+        res.status(500).json({
+            error : 'Internal server error.',
+        });
+    }
+}
+
+module.exports = registerController;
